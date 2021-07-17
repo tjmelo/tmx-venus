b9bbf21f3937ed8439b76f652daff31491010bcc
Thiago Melo <thiago.tmwd@gmail.com>
2021-07-17 13:39:29 -0300

    Refactory rules

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 mkdir build
 cp ./src/index.html ./src/index.js ./src/css/style.min.css build/
+cp -r ./src/images/ build/
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 
-
-
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-/*
- * We've enabled MiniCssExtractPlugin for you. This allows your app to
- * use css modules that will be moved into a separate CSS file instead of inside
- * one of your module entries!
- *
- * https://github.com/webpack-contrib/mini-css-extract-plugin
- *
- */
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-
-
-
-/*
- * We've enabled TerserPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/terser-webpack-plugin
- *
- */
 
 const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
-
 
 module.exports = {
   mode: 'development',
