f28319e2eaef01152065a1052346df439e652dcb
Thiago Melo <thiago.tmwd@gmail.com>
2022-03-16 21:08:41 -0300

    Data for api

--- dist/backend/functions/api.js
+++ dist/backend/functions/api.js
@@ -1,8 +1,8 @@
-// import data from '../../config/db.json';
+import data from '../../config/db.json';
 
 exports.handler = async function(event, context){
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
+        body: JSON.stringify(data)
     }
 }
